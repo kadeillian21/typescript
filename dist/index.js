6b6edbfcbdd806a30a3355fa66aeb08cbbd1c7aa
Kade Illian <kadeillianmt@gmail.com>
2022-10-20 19:41:16 -0600

    completed mosh tutorial

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,27 @@ let age = 20;
 if (age < 50)
     age += 10;
 console.log(age);
+let sale = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+function render(document) {
+    console.log(document);
+}
+function calculateTax(income, taxYear) {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(10000, 2022);
+let employee = { id: 1, name: "Kade", retire: (date) => { console.log(date); } };
+function kgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
+kgToLbs(10);
+kgToLbs("10kg");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,63 @@ function render(document: any) {
 
 
 // This is a properly annotated perameter.  Be sure to anotate what your expected return is
-let income: number = 10000
-function calculateTax(income: number): number {
-  if (income < 50_000)
+function calculateTax(income: number, taxYear: number): number {
+  if (taxYear < 2022)
     return income * 1.2;
   return income * 1.3;
 }
+
+calculateTax(10_000, 2022)
 //The above function would not compile correctly if we did not have the second return.  If we did not have the second return and we gave our condition a falsy value, then TS would return undefinded, whcih does not work because TS was expecting income to be a number, not undefined.
+// Adding a question mark after a variable in a function signifies that is is optional
+
+// Undry Objects
+let employee: {
+  readonly id: number,
+  name: string,
+  retire: (date: Date) => void
+} = {id: 1, name: "Kade", retire: (date: Date) => {console.log(date)}};
+// Read Only makes the property immutable
+
+// Dry Objects
+type Employee = {
+  readonly id: number,
+  name: string,
+  retire: (date: Date) => void
+}
+// This 'type-alias' creates a new data type that you can annotate variables with to make new instances of the object
+
+function kgToLbs(weight: number | string): number {
+  if (typeof weight === 'number')
+    return weight * 2.2;
+  else {
+    return parseInt(weight) * 2.2;
+  } 
+}
+
+kgToLbs(10);
+kgToLbs("10kg");
+
+
+// Intersection
+type Draggable = {
+  drag: () => void
+};
+
+type Resizable = {
+  resize: () => void
+};
+
+type UIWidget = Draggable & Resizable
+
+let textBox: UIWidget = {
+  drag: () => {},
+  resize: () => {}
+};
+
+// Literal (exact, specific) Types
+type Quantity = 50 | 100;
+let quantity: Quantity = 100;
+
+type Metric = 'cm' | 'inch';
+
